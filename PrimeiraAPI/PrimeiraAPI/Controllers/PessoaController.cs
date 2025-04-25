e2b1a7f1bb44b934ad698fa1631371d8410eb742
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-04-24 23:51:54 -0300

    Consegui criar e entender como funciona minha primeira API,

---
+++ PrimeiraAPI/PrimeiraAPI/Controllers/PessoaController.cs
@@ -0,0 +1,24 @@
+﻿using CadastroAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CadastroAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController:ControllerBase
+    {
+        [HttpGet("ListarPessoas")]
+        public IEnumerable<Pessoa> ListarPessoas()
+        {
+            var lista = new List<Pessoa>
+            {
+                new Pessoa {Id = 1, Nome = "Lucas", Idade = 27, Cpf = "44003132053",  Email = "lucas@email.com"},
+                 new Pessoa { Id = 2, Nome = "Mariana", Idade = 31, Cpf = "11223344556", Email = "mariana@email.com" }
+            };
+
+            return lista;
+        }
+        
+    }
+}
