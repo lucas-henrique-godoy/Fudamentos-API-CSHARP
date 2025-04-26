9cc4a4fce950e29113f62deaf4b66918940dd4a1
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-04-26 00:04:34 -0300

    Update PessoaController.cs

--- PrimeiraAPI/PrimeiraAPI/Controllers/PessoaController.cs
+++ PrimeiraAPI/PrimeiraAPI/Controllers/PessoaController.cs
@@ -14,7 +14,7 @@ namespace CadastroAPI.Controllers
             var lista = new List<Pessoa>
             {
                 new Pessoa {Id = 1, Nome = "Lucas", Idade = 27, Cpf = "44003132053",  Email = "lucas@email.com"},
-                 new Pessoa { Id = 2, Nome = "Mariana", Idade = 31, Cpf = "11223344556", Email = "mariana@email.com" }
+                new Pessoa { Id = 2, Nome = "Mariana", Idade = 31, Cpf = "11223344556", Email = "mariana@email.com" }
             };
 
             return lista;
